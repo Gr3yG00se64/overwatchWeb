a08a9dcbe71d38754ea3bac2064f6f82f4ea327b
Gr3yG00se64 <Gr3yG00se@protonmail.com>
2020-04-05 23:05:55 -0400

    Trying to fix index.js again

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use('/api/modules', modules);
 // Handle Production
 if(process.env.NODE_ENV === 'production') {
     // Static Folder
-    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public/'));
 
     // Handle SPA
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
