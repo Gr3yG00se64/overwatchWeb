c4c2fb9d7bdf9c6d7ec2772d035001b27205eb33
Gr3yG00se64 <Gr3yG00se@protonmail.com>
2020-04-05 23:52:07 -0400

    Further cleaned up modules routing file

--- server/routes/api/modules.js
+++ server/routes/api/modules.js
@@ -6,10 +6,9 @@ const router = express.Router();
 
 var db = require('../../db/interactions');
 
-//Data Directory Path
-
 //Module JSON path
 var full_path = path.resolve('server', 'data', 'mod_data.json')
+
 var mod_data = require(full_path);
 
 //Get Modules
@@ -20,14 +19,12 @@ router.get('/', (req, res) => {
 //Modify Module Activity Status
 router.post('/', (req, res) => {
     var i;
-    console.log(req.body);
     for(i=0; i < mod_data.modules.length; i++) {
         if (mod_data.modules[i].id == req.body.id) {
             mod_data.modules[i].enabled = !mod_data.modules[i].enabled;
         }
     }
 
-    console.log(JSON.stringify(mod_data));
     fs.writeFile(full_path, JSON.stringify(mod_data), 'utf8', function (err, data) {
         if(err) {console.log('error', err)}
 
